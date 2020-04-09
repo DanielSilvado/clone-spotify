198e5cf63459672b2f3b54dc279cb916e4a0eee1
DanielSilvado <41308458+DanielSilvado@users.noreply.github.com>
2020-04-09 17:10:19 -0300

    progress menu

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "../Player/styles";
+
+const Header = () => (
+  <Container>
+    <Search>
+      <input placeholder="Search" />
+    </Search>
+
+    <User>
+      <img src="" alt="" />
+    </User>
+  </Container>
+);
--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import Slider from "rc-slider";
-import { Container, Current, Volume } from "./styles";
+import {
+  Container,
+  Current,
+  Volume,
+  Controls,
+  Progress,
+  Time,
+  ProgressSlider,
+} from "./styles";
 
 import VolumeIcon from "../../assets/assets/images/volume.svg";
+import ShuffleIcon from "../../assets/assets/images/shuffle.svg";
+import BackwardIcon from "../../assets/assets/images/backward.svg";
+import PlayIcon from "../../assets/assets/images/play.svg";
+// import PauseIcon from "../../assets/assets/images/pause.svg";
+import ForwardIcon from "../../assets/assets/images/forward.svg";
+import RepeatIcon from "../../assets/assets/images/repeat.svg";
 
 const Player = () => (
   <Container>
@@ -16,13 +30,42 @@ const Player = () => (
         <small>Nivea Soares</small>
       </div>
     </Current>
-
+    <Progress>
+      <Controls>
+        <button>
+          <img src={ShuffleIcon} alt="Shuffle" />
+        </button>
+        <button>
+          <img src={BackwardIcon} alt="Backward" />
+        </button>
+        <button>
+          <img src={PlayIcon} alt="Play" />
+        </button>
+        <button>
+          <img src={ForwardIcon} alt="Forward" />
+        </button>
+        <button>
+          <img src={RepeatIcon} alt="Repeat" />
+        </button>
+      </Controls>
+      <Time>
+        <span>1:39</span>
+        <ProgressSlider>
+          <Slider
+            railStyle={{ background: "#404040", borderRadius: 10 }}
+            trackStyle={{ background: "#1ED760" }}
+            handleStyle={{ boder: 0 }}
+          />
+        </ProgressSlider>
+        <span>4:24</span>
+      </Time>
+    </Progress>
     <Volume>
       <img src={VolumeIcon} alt="Volume" />
       <Slider
         railStyle={{ background: "#404040", borderRadius: 10 }}
         trackStyle={{ background: "#fff" }}
-        handleStyle={{display: 'none'}}
+        handleStyle={{ display: "none" }}
         // value={100}
       />
     </Volume>

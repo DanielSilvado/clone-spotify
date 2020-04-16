968010394dfafe9d4ef6ca1a4ad9e4a6b919cac9
daniel.grisolia_storm <daniel.grisolia_storm@prestadortvg.com.br>
2020-04-16 18:15:01 -0300

    component header

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import GlobalStyle from "./styles/global";
 
 import Sidebar from "./components/Sidebar/index.jsx";
 import Player from "./components/Player/index.jsx";
+import Header from "./components/Header/index.jsx";
 
-import { Wrapper, Container } from "./styles/components";
+import { Wrapper, Container, Content } from "./styles/components";
 
 function App() {
   return (
     <Wrapper>
       <Container>
         <Sidebar />
+        <Content>
+          <Header />
+        </Content>
       </Container>
       <Player />
       <GlobalStyle />
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "../Player/styles";
+import { Container, Search, User } from './styles'
 
 const Header = () => (
   <Container>
@@ -8,7 +8,11 @@ const Header = () => (
     </Search>
 
     <User>
-      <img src="" alt="" />
+      <img src="https://avatars2.githubusercontent.com/u/41308458?s=460&u=6d583943571abe3a0cc0ba1e47f38c0ab9d096a8&v=4" alt="Avatar" />
+      Daniel Silvado
     </User>
   </Container>
 );
+
+
+export default Header;
